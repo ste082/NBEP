91bf9264e47870a1b2b53056d785a4ecf465bfe5
Stefano Cerrone <stefano.cerrone@finconsgroup.com>
2019-02-13 12:12:01 +0100

    Commit iniziale

---
+++ controller/src/main/java/it/posteitaliane/nbep/controller/ControllerApplication.java
@@ -0,0 +1,16 @@
+package it.posteitaliane.nbep.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+
+}
+
